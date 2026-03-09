f4f6c3c81ef59b77240aad9bd645ae573725e5f1
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2026-03-08 22:47:32 -0700

    Cleanup

--- components/ThemeControlForm/ThemeControlForm.tsx
+++ components/ThemeControlForm/ThemeControlForm.tsx
@@ -4,13 +4,16 @@ import { useState } from 'react';
 import { Slider } from '../ui/slider';
 
 export const ThemeControlForm = () => {
-  const [hue, setHue] = useState(180);
-  const [saturation, setSaturation] = useState(100);
-  const [lightness, setLightness] = useState(50);
+  const [values, setValues] = useState({
+    hue: 180,
+    saturation: 100,
+    lightness: 50,
+  });
+  const { hue, saturation, lightness } = values;
 
   return (
     <div className="grid gap-4">
-      <fieldset className="flex gap-4 items-center justify-center">
+      {/* <fieldset className="flex gap-4 items-center justify-center">
         <span>Hue</span>
         <Slider
           value={saturation}
@@ -41,7 +44,26 @@ export const ThemeControlForm = () => {
           onValueChange={(value) => setLightness(value as number)}
           className="w-full"
         />
-      </fieldset>
+      </fieldset> */}
+      {Object.entries(values).map(([key, value], index) => (
+        <fieldset key={key} className="grid gap-1">
+          <label>
+            {[key.slice(0, 1).toUpperCase(), key.slice(1)].join('')}
+          </label>
+          <Slider
+            className="w-full"
+            value={value}
+            max={index ? 100 : 360}
+            step={1}
+            onValueChange={(changeVal) =>
+              setValues((curr) => ({
+                ...curr,
+                [key]: changeVal,
+              }))
+            }
+          />
+        </fieldset>
+      ))}
       <div
         className="w-14 aspect-video rounded"
         style={{ backgroundColor: `hsl(${hue} ${saturation}% ${lightness}%)` }}
